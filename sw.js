68bfd495e2bcfae157a27e02da4e4173c2bd3b06
Bryant Loy <bryant.r.loy@gmail.com>
2026-09-20 13:07:30 -0500

    V2

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE="loy-travel-v1";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
-    const copy=r.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return r;
-  }).catch(()=>caches.match("./index.html"))));
-});
+const C="loy-v02",A=["/","/index.html","/styles.css","/app.js","/ideas.json","/manifest.json","/icons/icon-192.png","/icons/icon-512.png","/icons/apple-touch-icon.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>{if(e.request.method!=="GET"||e.request.url.includes("/api/"))return;e.respondWith(caches.match(e.request).then(h=>h||fetch(e.request)))})
